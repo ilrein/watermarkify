a20c9b77b72e44e7af7a8a3825b672bc76c72281
merLO <iliareingold@gmail.com>
2017-05-04 12:15:39 -0400

    plz work

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 
+import Hero from './ui/hero';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        test
+        <Hero />
       </div>
     );
   }
---
+++ src/ui/hero/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+import styles from './styles.scss';
+
+const Hero = () =>
+  <div className={styles.hero}>hero</div>;
+
+export default Hero;
