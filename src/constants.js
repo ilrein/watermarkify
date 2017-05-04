f9cd65df4c57dee5eba31bbb33b0c3790754fa63
merLO <iliareingold@gmail.com>
2017-05-04 13:31:55 -0400

    added hero with gradient

---
+++ src/constants.js
@@ -0,0 +1,2 @@
+export const APP_NAME = 'Watermarkify';
+export const APP_DESCRIPTION = 'Easily add watermarks to any of your product photos';
--- src/ui/hero/index.js
+++ src/ui/hero/index.js
@@ -1,4 +1,10 @@
 import React, { Component } from 'react';
+import { Header, Icon } from 'semantic-ui-react';
+
+import {
+  APP_NAME,
+  APP_DESCRIPTION,
+} from '../../constants';
 
 import styles from './styles.scss';
 
@@ -10,7 +16,13 @@ class Hero extends Component {
   render() {
     return (
       <div className={styles.hero}>
-        {this.state.redirectUri}
+        <Header className={styles.content} as="h1">
+          {APP_NAME}
+          <Header.Subheader>
+            {APP_DESCRIPTION}
+          </Header.Subheader>
+        </Header>
+        <Icon name="arrow down" className={styles.icon} size="large" />
       </div>
     );
   }
