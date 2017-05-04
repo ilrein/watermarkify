2a669f72b3de2f1725d2db9d62bef6c7f46ab9e0
merLO <iliareingold@gmail.com>
2017-05-04 12:24:44 -0400

    added shopify-token

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
-
+import ShopifyToken from 'shopify-token';
 import 'semantic-ui-css/semantic.min.css';
 
 import Hero from './ui/hero';
 
 class App extends Component {
+  componentWillMount() {
+    console.log(ShopifyToken);
+  }
   render() {
     return (
       <div className="App">
