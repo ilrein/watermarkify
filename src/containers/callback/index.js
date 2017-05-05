52885009f84c934c5aeb2c7d3a1ae16defe15f64
merLO <iliareingold@gmail.com>
2017-05-05 11:30:25 -0400

    added firebase cloud func

--- src/containers/callback/index.js
+++ src/containers/callback/index.js
@@ -3,7 +3,6 @@
 //
 import React, { Component } from 'react';
 import queryString from 'query-string';
-import fetch from 'isomorphic-fetch';
 import { connect } from 'react-redux';
 
 class Callback extends Component {
@@ -20,35 +19,21 @@ class Callback extends Component {
       this.setState({
         securityChecksPassed: checkPassed,
       }, () => {
-        shopifyToken.getAccessToken(query.shop, query.code)
-          .then((token) => {
-            console.log('got token', token);
-          })
-          .catch((err) => {
-            console.log('err', err);
-          })
-          
-        // const url = `https://${query.shop}/admin/oauth/access_token`;
-        // fetch(url, {
-        //   method: 'POST',
-        //   headers: {
-        //     code: query.code,
-        //     hmac: query.hmac,
-        //     shop: query.shop,
-        //     state: query.state,
-        //     timestamp: query.timestamp,
-        //     'Accept': 'application/json',
-        //     'Content-Type': 'application/json'
-        //   },
-        //   body: JSON.stringify({
-        //     client_id: process.env.REACT_APP_API_KEY,
-        //     client_secret: process.env.REACT_APP_SHARED_SECRET,
-        //     code: query.code,
+        /**
+         * We will need to use a cloud function here
+         */
+        console.log('checks passed...');
+        fetch('https://us-central1-watermarkify.cloudfunctions.net/helloWorld')
+          .then(response => response.json())
+          .then(json => console.log(json))
+          .catch(e => console.log(e));
+        // shopifyToken.getAccessToken(query.shop, query.code)
+        //   .then((token) => {
+        //     console.log('got token', token);
         //   })
-        // })
-        // .then(data => data.json())
-        // .then(json => console.log(json))
-        // .catch(e => console.log('error!', e));
+        //   .catch((err) => {
+        //     console.log('err', err);
+        //   })
       });
     }
   }
